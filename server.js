7e9e9333a38153001742a6f5aefdf81750508ea8
manhkun95 <nguyenduymanh0406@gmail.com>
2018-08-03 14:26:51 +0700

    fix view engine

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const bodyParser  = require('body-parser');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
 
 const app = express();
-app.set('view engine', 'pug');
 fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'pug');
 
 app.route('/')
   .get((req, res) => {
